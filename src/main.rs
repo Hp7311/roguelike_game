bd41f2173e9c084417c6b73e05d6dda7bb855a29
Hp7311 <williammagical83@gmail.com>
2025-12-23 20:40:56 +0000

    Adds functionality for player to move, no monsters, large ownership errors

---
+++ src/main.rs
@@ -0,0 +1,120 @@
+use crossterm::event::{read, Event, KeyCode};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use crossterm::execute;
+
+mod map;
+mod entities;
+
+#[derive(Debug)]
+enum RunState {
+    PreRun,
+    AwaitingInput,
+    PlayerTurn,
+    MonsterTurn,
+    GameOver,
+}
+
+#[derive(Debug)]
+struct State {
+    // Map (Vec<Vec<Tile>>)
+    // Tile enum: Wall, Floor, Player, Monster
+    map: map::Map,
+    
+    runstate: RunState,
+}
+
+impl State {
+    fn render(&self) {
+        //execute!(terminal::Clear(terminal::ClearType::All));
+        self.map.draw();
+        // TODO add drawing player stats
+    }
+
+    /*fn run_monsters(&mut self) {
+        self.map = self.map.handle_monsters();
+    }*/
+    
+    fn move_player(&mut self, to: char) {
+        self.map = self.map.move_player(to);
+    }
+    
+    /*fn run_player(&self) {
+        self.map.handle_player()
+    }
+    
+    fn get_death_screen(&self) {
+        self.map.get_player_stats()
+    }*/
+}
+
+fn main() -> Result<(), std::io::Error> {
+
+    // --- INITIALIZATION --
+    enable_raw_mode()?;
+    
+    let mut gs = State {
+        map: map::init_map(),
+        runstate: RunState::PreRun,
+    };
+
+    // --- CORE GAME LOOP ---
+    loop {
+        match gs.runstate {
+            RunState::PreRun => {
+                gs.runstate = RunState::AwaitingInput;
+            }
+            RunState::AwaitingInput => {
+                let mut key = 'a';
+                loop {
+                    if let Event::Key(event) = read()? {
+                        if let KeyCode::Char(c) = event.code {
+                            key = c;
+                            break;
+                        }
+                    }
+                }
+                if key == 'q' { break }
+                match key {
+                    'w' | 'a' | 's' | 'd' => {
+                        gs.map = gs.map.move_player(key);
+                        gs.runstate = RunState::PlayerTurn;
+                    },
+                    _ => {},
+                }
+            }
+            RunState::PlayerTurn => {
+                //gs.run_player();
+                gs.runstate = RunState::MonsterTurn;
+            }
+            RunState::MonsterTurn => {
+                //gs.run_monsters();
+                gs.runstate = RunState::AwaitingInput;
+            }
+            RunState::GameOver => {
+                //gs.get_death_screen();
+                // TODO wait for restart/quit
+                let mut key = 'a';
+                loop {
+                    if let Event::Key(event) = read()? {
+                        if let KeyCode::Char(c) = event.code {
+                            key = c;
+                            break;
+                        }
+                    }
+                    match key {
+                        'q' => break,
+                        'r' => gs.runstate = RunState::PreRun,
+                        _ => {},
+                    }
+                }
+                
+            }
+        }
+
+        // --- THE RENDER STEP ---
+        gs.render();
+    }
+    disable_raw_mode()?;
+    
+    Ok(())
+}
---
+++ src/map.rs
@@ -0,0 +1,141 @@
+// provides Map
+use crate::entities::Tile;
+use crate::entities;
+use rand::Rng;
+
+#[derive(Debug)]
+pub struct Map(Vec<Vec<Tile>>);
+
+
+impl Map {
+    pub fn draw(&self) {
+        for small in &self.0 {
+            for tile in small {
+                match tile {
+                    Tile::Wall => print!("#"),
+                    Tile::Player => print!("@"),
+                    Tile::Monster => print!("M"),  // TODO add more
+                    _ => {},
+                }
+            }
+            println!("");
+        }
+    }
+    
+    /*pub fn handle_monsters(&self) -> Self {
+        return_map = Map(init_map)
+        for small in &self.0 {
+            for tile in small {
+                if tile == Tile::Monster {
+                    if entities::monster_can_see_you(self) {
+                        entities::monster_move_to_player(self);
+                    }
+                }
+                
+            }
+        }
+    }*/
+    
+    pub fn move_player(&self, to: char) -> Self {
+        // moves player to a direction and returns new map
+        let mut return_vec = self.0;
+        for (a, inner) in return_vec.iter().enumerate() {
+            for (i, tile) in inner.iter().enumerate() {
+                if *tile == Tile::Player {
+                    match to {
+                        'w' => {
+                            if return_vec[a-1][i] == Tile::Floor {
+                                return_vec[a][i] = Tile::Floor;
+                                return_vec[a-1][i] = Tile::Player;
+                            }
+                        },
+                        'a' => {
+                            if return_vec[a][i-1] == Tile::Floor {
+                                return_vec[a][i] = Tile::Floor;
+                                return_vec[a][i-1] = Tile::Player;
+                            }
+                        },
+                        's' => {
+                            if return_vec[a+1][i] == Tile::Floor {
+                                return_vec[a][i] = Tile::Floor;
+                                return_vec[a+1][i] = Tile::Player;
+                            }
+                        },
+                        'd' => {
+                            if return_vec[a][i+1] == Tile::Floor {
+                                return_vec[a][i] = Tile::Floor;
+                                return_vec[a][i+1] = Tile::Player;
+                            }
+                        },
+                        _ => {},
+                    }
+                }
+            }
+        }
+        
+        Map (return_vec)
+    }
+    
+    //pub fn handle_player(&self) {}
+    //pub fn get_player_stats(&self)
+    
+    fn dig_floors(&self) -> Self {
+        let mut return_vec = Vec::new();
+        
+        /*const WIDTH: i32 = self.0.len();
+        const LENGTH: i32 = self.0.0.len();*/
+        
+        for inner in &self.0 {
+            return_vec.push(Vec::new());
+            let index: usize = return_vec.len() - 1;
+            
+            for tile in inner {
+                
+                // TODO add corridors, not random
+                let b: bool = rand::random();
+                if b {
+                    return_vec[index].push(Tile::Floor);
+                } else {
+                    return_vec[index].push(Tile::Wall);
+                }
+            }
+        }
+        
+        Map (return_vec)
+    }
+    
+    fn add_player(&self) -> Self {
+        let mut return_vec = self.0.clone();
+        let x = rand::rng().random_range(..return_vec.len());
+        let y = rand::rng().random_range(..return_vec[0].len());
+        return_vec[x][y] = Tile::Player;
+        Map (return_vec)
+    }
+}
+
+pub fn init_map() -> Map {
+    /*   length
+      |---------|
+      
+      -
+      |
+ width|
+      |
+      -
+    */
+    const LENGTH: i32 = 10;
+    const WIDTH: i32 = 10;
+    let mut return_vec = Vec::new();
+    
+    for inner_vec in 0..WIDTH {
+        return_vec.push(Vec::new());
+        for _ in 0..LENGTH {
+            return_vec[inner_vec as usize].push(Tile::Wall);
+        }
+    }
+    
+    Map (return_vec)
+        .dig_floors()
+        .add_player()
+    
+}
